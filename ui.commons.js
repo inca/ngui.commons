79dcb8695b09d5c56a3bc36c872f8b35fffb2a6e
Boris Okunskiy <i@savant.pro>
2014-11-20 17:00:38 +0300

    Initialized a module.

---
+++ ui.commons.js
@@ -0,0 +1,32 @@
+(function(window, document, angular, undefined) {
+
+  var module = angular.module('ui.commons', []);
+
+  module.directive('uiHl', function() {
+    return {
+      restrict: 'AE',
+      link: function ($scope, $element, $attrs) {
+        var className = $attrs.uiHlClass || 'active';
+        var nodes = document.querySelectorAll($attrs.uiHl);
+        [].forEach.call(nodes, function(node) {
+          angular.element(node).addClass(className);
+        });
+        $element.on('$destroy', function() {
+          [].forEach.call(nodes, function(node) {
+            angular.element(node).removeClass(className);
+          });
+        });
+      }
+    }
+  });
+
+  module.directive('uiFocus', function() {
+    return {
+      restrict: 'A',
+      link: function ($scope, $element, $attrs) {
+        $element[0].focus();
+      }
+    }
+  });
+
+})(window, document, angular);
